a3ebcba2bf3c9635092d1f94efbb022f7cffdd5a
William Gasper <wigasper@gmail.com>
2018-12-24 14:52:18 -0600

    Basic style changes

--- pull_XMLs.py
+++ pull_XMLs.py
@@ -18,24 +18,25 @@ for ID in tqdm(ids_to_get['Accession ID']):
     Entrez.email = "kgasper@unomaha.edu"
     handle = Entrez.efetch(db="pmc", id=ID, retmode="xml")
     xmlString = handle.read()
-
     element = xmltodict.parse(xmlString)
-    
+
     pmc_error = False
-    
+
     # Check for an error on PMC's side and record it
     for key in element['pmc-articleset'].keys():
-        if (key == 'error'):
+        if key == 'error':
             refs = element['pmc-articleset']['error']
-            pmcErrorData = {'ID': [ID], 'code': [refs['Code']], 'message': [refs['Message']]}
-            tempPMCerrorDF = pd.DataFrame(pmcErrorData, columns=['ID', 'code', 'message'])
+            pmcErrorData = {'ID': [ID], 'code': [refs['Code']],
+                            'message': [refs['Message']]}
+            tempPMCerrorDF = pd.DataFrame(pmcErrorData, columns=['ID', 'code',
+                                                                 'message'])
             PMC_errors = PMC_errors.append(tempPMCerrorDF, ignore_index=True)
             pmc_error = True
-    
+
     if not pmc_error:
         file_out = open("./PMC XMLs/{}.xml".format(ID), "w")
         file_out.write(xmlString)
-    
+
     # This is a delay in accordance with PubMed API usage guidelines.
     # It should not be set lower than .34.
     time.sleep(.4)
