cb326334695bf4dee83ba484d042003c5f0e2aab
William Gasper <wigasper@gmail.com>
2018-12-29 20:07:19 -0600

    Normal working update

---
+++ get_mesh_terms.py
@@ -0,0 +1,25 @@
+# sandbox for prototyping getting of MeSH terms
+# how do I deal with qualifiers?
+
+from Bio import Entrez
+from bs4 import BeautifulSoup
+
+
+ID = "10073946"
+Entrez.email = "kgasper@unomaha.edu"
+handle = Entrez.efetch(db="pubmed", id=ID, retmode="xml")
+#handle = open("./PMC XMLs/{}.xml".format(ID), "r")
+
+soup = BeautifulSoup(handle.read())
+
+mesh_terms = []
+
+for mesh_heading in soup.find_all("meshheading"):
+    if mesh_heading.descriptorname != None:
+        term_id = mesh_heading.descriptorname['ui']
+        if mesh_heading.qualifiername != None:
+            for qualifier in mesh_heading.find_all("qualifiername"):
+                term_id = "".join([term_id, ",", qualifier['ui']])
+    mesh_terms.append(term_id)
+    
+print(mesh_terms)
--- get_refs_beautiful_soup.py
+++ get_refs_beautiful_soup.py
@@ -48,7 +48,7 @@ mti_refs = pd.DataFrame(mti_refs)
 # mti_refs.to_csv('mti_refs_w_DOIs.csv')
 
 # Read in if needed
-mti_refs = pd.read_csv("mti_refs_w_DOIs.csv", low_memory=False)
+# mti_refs = pd.read_csv("mti_refs_w_DOIs.csv", low_memory=False)
 
 # Read in PMC_IDs to convert all the DOIs to PMIDs:
 PMC_ids = pd.read_csv("PMC-ids.csv", low_memory=False)
@@ -64,12 +64,7 @@ DOI_PMIDs.PMID = DOI_PMIDs.PMID.fillna(0)
 DOI_PMIDs.PMID = DOI_PMIDs.PMID.astype(int).astype(str)
 DOI_PMIDs.PMID = DOI_PMIDs.PMID.replace("0", "NA")
 
-
-## 10.1111/j.1365-4362.1994.tb02872.x
-#result = DOI_PMIDs[DOI_PMIDs.DOI == "10.1111/j.1365-4362.1994.tb02872.x"].PMID
-#result = DOI_PMIDs[DOI_PMIDs.DOI == "10.1016/j.ijid.2008.06.019"].PMID
-
-# Find DOIs and convert them to PMIDs if possible. check col 4/93
+# Find DOIs and convert them to PMIDs if possible
 for row in tqdm(range(0, len(mti_refs))):
     for col in range(0, len(mti_refs.columns)):
         if re.match(r"^[1][0][.]..*$", str(mti_refs.iloc[row, col])):
@@ -80,14 +75,14 @@ for row in tqdm(range(0, len(mti_refs))):
                 mti_refs.iloc[row, col] = np.NaN
 
 # Save if needed:
-mti_refs.to_csv("mti_refs_wo_DOIs.csv")
+# mti_refs.to_csv("mti_refs_wo_DOIs.csv")
 
 # Read in if needed:
 # mti_refs = pd.read_csv("mti_refs_wo_DOIs.csv", low_memory=False)
-# mti_refs = mti_refs.drop("Unnamed: 0", axis=1)
+# mti_refs = mti_refs.loc[:, ~mti_refs.columns.str.contains('^Unnamed')]
             
 # Remove IDs in the format "2-s......." 3499
-mti_refs = mti_refs.replace("^2-s..*$", np.NaN, regex=True)
+mti_refs = mti_refs.replace("^2[-]s..*$", np.NaN, regex=True)
 
 # Make edge list by melting the DF. Drop unnecessary column and NAs
 edge_list = pd.melt(mti_refs, id_vars=['0'], 
@@ -102,30 +97,6 @@ edge_list = edge_list.sort_values(by=['0'])
 edge_list = edge_list.drop_duplicates()
 edge_list.to_csv("edge_list.csv")
 
-#
-#get_refs_doi_to_pmid_run1 = pd.read_csv("get_refs_doi_to_pmid_run1.csv", low_memory=False)
-#if tester.equals(mti_refs_short[1:250]):
-#    print("yes they are the same")
-# last run made:
-# 18%|█▊        | 1830/10329 [3:28:01<29:06:20, 12.33s/it] on 12/22 22:30
-        
-#repr(mti_refs_short3.iloc[[46], [2]])
-#test = get_PMID_from_DOI(mti_refs_short.iloc[66, 2])
-if re.match("^[2][-][s]..*$", "2-s2.0-77954665047"):
-    print("oh yeah")
-if not re.match("^[2][-][s]..*$", "2-s2.0-77954665047"):
-    print("nope, not at all")
-
-if re.match("^[1][0][.]..*$", "10.1006/pmed.2000.0731.S009174350090731X"):
-    print("oh yeah")
-if not re.match("^[1][0][.]..*$", "10.1006/pmed.2000.0731.S009174350090731X"):
-    print("nope, not at all")
-    
-if re.match("^..*$", "10.1006/pmed.2000.0731.S009174350090731X"):
-    print("oh yeah")
-if not re.match("^.*10\..*$", "10.1006/pmed.2000.0731.S009174350090731X"):
-    print("nope, not at all")
-
 
 ##############################################
 # From first run:
@@ -134,3 +105,5 @@ if not re.match("^.*10\..*$", "10.1006/pmed.2000.0731.S009174350090731X"):
 # PMC3339694, PMC3339697, PMC3363164, PMC3366896, PMC3371872, PMC3374144
 # PMC3376543, PMC3393343, PMC3398479, PMC3407656, PMC3409423, PMC3412676
 # PMC3446280, PMC3446285, PMC3496406, PMC4605518, PMC4605596, PMC4605686
+#
+# All FNFEs resolved
