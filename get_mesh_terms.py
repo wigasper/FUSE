70ea1cc8715324f349ceedca609ed6aa654f96d8
William Gasper <wigasper@gmail.com>
2019-01-07 09:29:38 -0600

    Normal updates

--- get_mesh_terms.py
+++ get_mesh_terms.py
@@ -4,7 +4,6 @@
 from Bio import Entrez
 from bs4 import BeautifulSoup
 
-
 ID = "10073946"
 Entrez.email = "kgasper@unomaha.edu"
 handle = Entrez.efetch(db="pubmed", id=ID, retmode="xml")
@@ -12,14 +11,30 @@ handle = Entrez.efetch(db="pubmed", id=ID, retmode="xml")
 
 soup = BeautifulSoup(handle.read())
 
-mesh_terms = []
+mesh_terms = [ID]
+
+#for mesh_heading in soup.find_all("meshheading"):
+#    if mesh_heading.descriptorname != None:
+#        #term_id = mesh_heading.descriptorname['ui']
+#        term_id = mesh_heading.descriptorname.string
+#        if mesh_heading.qualifiername != None:
+#            for qualifier in mesh_heading.find_all("qualifiername"):
+#                #term_id = "".join([term_id, ",", qualifier['ui']])
+#                term_id = "".join([term_id, "/", qualifier.string])
+#    mesh_terms.append(term_id)
+    
 
 for mesh_heading in soup.find_all("meshheading"):
     if mesh_heading.descriptorname != None:
-        term_id = mesh_heading.descriptorname['ui']
+        #term_id = mesh_heading.descriptorname['ui']
+        term_id = mesh_heading.descriptorname.string
         if mesh_heading.qualifiername != None:
             for qualifier in mesh_heading.find_all("qualifiername"):
-                term_id = "".join([term_id, ",", qualifier['ui']])
-    mesh_terms.append(term_id)
+                #term_id = "".join([term_id, ",", qualifier['ui']])
+                #term_id = "".join([term_id, "/", qualifier.string])
+                mesh_terms.append("".join([term_id, "/", qualifier.string]))
+        else:
+            mesh_terms.append(term_id)
+    #mesh_terms.append(term_id)
     
 print(mesh_terms)
