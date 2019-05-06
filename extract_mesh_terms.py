b12b1878b5db8534f56460363846a13b4228b0d8
Kirk Gasper <wigasper@gmail.com>
2019-05-06 10:23:37 -0500

    comment edit

--- extract_mesh_terms.py
+++ extract_mesh_terms.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
 
 import logging
+import json
 
 from bs4 import BeautifulSoup
 from tqdm import tqdm
@@ -22,7 +24,7 @@ with open("./data/edge_list.csv", "r") as handle:
         
         doc_refs_dict[line[0]].append(line[1])
         
-        # Term extraction is the time limiting step, with a little extra work
+        # Term extraction is the time bottleneck, with a little extra work
         # they can be put into their own list, delete duplicates, create a dict
         # to save some minutes
         ids_to_get.append(line[1])
@@ -30,9 +32,11 @@ with open("./data/edge_list.csv", "r") as handle:
 # Drop duplicates
 ids_to_get = list(dict.fromkeys(ids_to_get))
 
+# Create a dict to store the MeSH terms for each PMID
 doc_term_dict = {}
 
-for pmid in tqdm(ids_to_get[0:500]):
+# Extract MeSH terms for each PMID
+for pmid in tqdm(ids_to_get):
     try:
         with open("./MeSH XMLs/{}.xml".format(pmid), "r") as handle:
             soup = BeautifulSoup(handle.read())
@@ -53,15 +57,18 @@ for pmid in tqdm(ids_to_get[0:500]):
 term_counts = []
 
 for doc in doc_refs_dict.keys():
-    doc_counts = {}
-    for ref in doc_refs_dict[doc]:
-        for term in doc_term_dict[ref]:
-            if term not in doc_counts.keys():
-                doc_counts[term] = 1
-            else:
-                doc_counts[term] += 1
-                
-    term_counts.append([doc, doc_counts])
+    try:
+        doc_counts = {}
+        for ref in doc_refs_dict[doc]:
+            for term in doc_term_dict[ref]:
+                if term not in doc_counts.keys():
+                    doc_counts[term] = 1
+                else:
+                    doc_counts[term] += 1
+                    
+        term_counts.append([doc, doc_counts])
+    except KeyError:
+        logger.error("KeyError at counts - PMID: {}".format(str(ref)))
 
 # Change counts to relative frequency
 for doc in term_counts:
@@ -70,3 +77,7 @@ for doc in term_counts:
         total_count += doc[1][term_id]
     for term_id in doc[1].keys():
         doc[1][term_id] = doc[1][term_id] / total_count
+
+with open("./data/train_term_counts.json", "w") as out:
+    json.dump(term_counts, out)
+        
