19fa83cedd40cd3e2ea30a94aa84e2ee35037b23
Kirk Gasper <wigasper@gmail.com>
2019-05-09 10:18:30 -0500

    commented out TN related code (not useful and time bottleneck)

--- baseline_model.py
+++ baseline_model.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
 import json
-from tqdm import tqdm
 
 # Load in term frequencies
 with open("./data/term_freqs.json", "r") as handle:
-    term_counts = json.load(handle)
+    term_freqs = json.load(handle)
 
 # Load in solution values
 with open("./data/baseline_solution.json", "r") as handle:
     solution = json.load(handle)
 
-# Get a list of all potential labels for evaluation against
+# Get a list of all potential labels for evaluation against (used for TNs)
 # This needs to eventually be expanded to include all MeSH terms
-descriptors = []
-
-for pmid in solution.keys():
-    for descriptor in solution[pmid]:
-        descriptors.append(descriptor)
-
-for sample in term_counts:
-    for descriptor in sample[1].keys():
-        descriptors.append(descriptor)
-
-descriptors = list(dict.fromkeys(descriptors))
+#descriptors = []
+#
+#for pmid in solution.keys():
+#    for descriptor in solution[pmid]:
+#        descriptors.append(descriptor)
+#
+#for sample in term_freqs:
+#    for descriptor in sample[1].keys():
+#        descriptors.append(descriptor)
+#
+#descriptors = list(dict.fromkeys(descriptors))
 
 # Run the baseline model. This model will predict a term if its frequency
 # is greater than the threshold
@@ -31,26 +30,26 @@ thresholds = [x * .005 for x in range(0,200)]
 predictions = {}
 precisions = []
 recalls = []
-accuracies = []
+#accuracies = []
 f1s = []
-tprs = []
-fprs = []
+#tprs = []
+#fprs = []
 
 # Run the model for all thresholds
 for thresh in thresholds:
     # Predict
-    for doc in term_counts:
+    for doc in term_freqs:
         predictions[doc[0]] = [key for key, val in doc[1].items() if val > thresh]
         
     # Get evaluation metrics
     true_pos = 0
     false_pos = 0
-    true_neg = 0
+    #true_neg = 0
     false_neg = 0
     
-    for pmid in tqdm(predictions.keys()):
+    for pmid in predictions.keys():
         true_pos += len([pred for pred in predictions[pmid] if pred in solution[pmid]])
-        true_neg += len([desc for desc in descriptors if desc not in solution[pmid] and desc not in predictions[pmid]])
+        #true_neg += len([desc for desc in descriptors if desc not in solution[pmid] and desc not in predictions[pmid]])
         false_pos += len([pred for pred in predictions[pmid] if pred not in solution[pmid]])
         false_neg += len([sol for sol in solution[pmid] if sol not in predictions[pmid]])
 
@@ -63,14 +62,14 @@ for thresh in thresholds:
         recall = true_pos / (true_pos + false_neg)
         f1 = (2 * precision * recall) / (precision + recall)
         
-    accuracy = (true_pos + true_neg) / (true_pos + true_neg + false_pos + false_neg)
+    #accuracy = (true_pos + true_neg) / (true_pos + true_neg + false_pos + false_neg)
     
     precisions.append(precision)
     recalls.append(recall)
-    accuracies.append(accuracy)
+    #accuracies.append(accuracy)
     f1s.append(f1)
-    tprs.append(true_pos / (true_pos + false_neg))
-    fprs.append(false_pos / (true_neg + false_pos))
+    #tprs.append(true_pos / (true_pos + false_neg))
+    #fprs.append(false_pos / (true_neg + false_pos))
 
 # Write evaluation metrics
 with open("./data/baseline_eval_metrics.csv", "w") as out:
@@ -78,10 +77,10 @@ with open("./data/baseline_eval_metrics.csv", "w") as out:
         out.write("".join([str(thresholds[index]), ","]))
         out.write("".join([str(precisions[index]), ","]))
         out.write("".join([str(recalls[index]), ","]))
-        out.write("".join([str(accuracies[index]), ","]))
-        out.write("".join([str(f1s[index]), ","]))
-        out.write("".join([str(tprs[index]), ","]))
-        out.write("".join([str(fprs[index]), "\n"]))
+        #out.write("".join([str(accuracies[index]), ","]))
+        out.write("".join([str(f1s[index]), "\n"]))
+        #out.write("".join([str(tprs[index]), ","]))
+        #out.write("".join([str(fprs[index]), "\n"]))
 
 # This code is for reading in evaluation metrics for later examination
 precisions = []
@@ -109,4 +108,21 @@ from matplotlib import pyplot
 print("AUC: ", auc(recalls, precisions))
 pyplot.plot([0, 1], [0.5, 0.5], linestyle="--")
 pyplot.plot(recalls, precisions, marker=".")
-pyplot.show()    
+pyplot.show()
+
+
+
+
+
+################################
+
+for doc in term_freqs:
+    for key, val in doc[1].items():
+        if val > .25:
+            print(doc[0])
+
+interesting_docs = ['22933841', '22943249', '23002304']
+test = []
+for doc in term_freqs:
+    if doc[0] in interesting_docs:
+        test.append(doc)
