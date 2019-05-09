edb6c5f523c3211b131616d670edfabeaa72b2bf
Kirk Gasper <wigasper@gmail.com>
2019-05-09 10:39:46 -0500

    playing with weights

--- simple_weighted_model.py
+++ simple_weighted_model.py
@@ -2,6 +2,9 @@
 
 import json
 
+from sklearn.metrics import auc
+from matplotlib import pyplot
+
 # Load in term frequencies
 with open("./data/term_freqs.json", "r") as handle:
     term_freqs = json.load(handle)
@@ -14,12 +17,12 @@ ui_depths = {}
 with open("./data/mesh_data.tab", "r") as handle:
     for line in handle:
         line = line.strip("\n").split("\t")
-        ui_depths[line[0]] = line[2]
+        ui_depths[line[0]] = int(line[2])
         # may also be able to use distinct_tree_posits here, from line[3]
 
-depths = [v for k,v in ui_depths.items()]
+#depths = [v for k,v in ui_depths.items()]
 def weight(ui, weight_dict=ui_depths):
-    
+    return weight_dict[ui] / 4
 # Run the baseline model. This model will predict a term if its frequency
 # is greater than the threshold
 thresholds = [x * .005 for x in range(0,200)]
@@ -33,7 +36,7 @@ f1s = []
 for thresh in thresholds:
     # Predict
     for doc in term_freqs:
-        predictions[doc[0]] = [key for key, val in doc[1].items() if val > thresh]
+        predictions[doc[0]] = [key for key, val in doc[1].items() if (weight(key) * val) > thresh]
         
     # Get evaluation metrics
     true_pos = 0
@@ -57,42 +60,10 @@ for thresh in thresholds:
     precisions.append(precision)
     recalls.append(recall)
     f1s.append(f1)
-
-# Write evaluation metrics
-with open("./data/baseline_eval_metrics.csv", "w") as out:
-    for index in range(len(thresholds)):
-        out.write("".join([str(thresholds[index]), ","]))
-        out.write("".join([str(precisions[index]), ","]))
-        out.write("".join([str(recalls[index]), ","]))
-        out.write("".join([str(f1s[index]), "\n"]))
-
-# This code is for reading in evaluation metrics for later examination
-precisions = []
-recalls = []
-accuracies = []
-f1s = []
-tprs = []
-fprs = []
-
-with open("./data/baseline_eval_metrics.csv", "r") as handle:
-    for line in handle:
-        line = line.strip("\n").split(",")
-        precisions.append(float(line[1]))
-        recalls.append(float(line[2]))
-        accuracies.append(float(line[3]))
-        f1s.append(float(line[4]))
-        tprs.append(float(line[5]))
-        fprs.append(float(line[6]))
         
-        
-from sklearn.metrics import auc
-from matplotlib import pyplot
-
 # AUC
 print("AUC: ", auc(recalls, precisions))
 pyplot.plot([0, 1], [0.5, 0.5], linestyle="--")
 pyplot.plot(recalls, precisions, marker=".")
 pyplot.show()
-
-
 
