9aa7fa633cc9562c1e4cc7f7fc6154cfa9868c80
Kirk Gasper <wigasper@gmail.com>
2019-05-16 13:58:20 -0500

    added semantic value compute

--- compute_term_aic.py
+++ compute_term_aic.py
@@ -9,8 +9,11 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 # Set up logging
+#logging.basicConfig(filename="errors.log", level=logging.ERROR,
+#                    format="%(asctime)s - %(levelname)s - %(modules)s: %(funcName)s - %(message)s",
+#                    datefmt="%d %b, %H%M")
 logging.basicConfig(filename="errors.log", level=logging.INFO,
-                    format="AIC Compute: %(levelname)s - %(message)s")
+                    format="Compute term AIC: %(levelname)s - %(message)s")
 logger = logging.getLogger()
 
 uids = []
@@ -26,8 +29,9 @@ with open("./data/mesh_data.tab", "r") as handle:
 
 docs = os.listdir("./mesh_xmls")
 
-# Create term_trees dict for quick and easy lookup later
+# Create term_trees dict and reverse for quick and easy lookup later
 term_trees = {uids[idx]:trees[idx] for idx in range(len(uids))}
+term_trees_rev = {tree:uids[idx] for idx in range(len(uids)) for tree in trees[idx]}
 
 #########################################
 # commented out for run
@@ -108,11 +112,11 @@ for term in term_freqs.keys():
 #    for term in term_freqs.items():
 #        out.write("".join([term[0], ",", str(term[1]), "\n"]))
 #
-#term_freqs = {}
-#with open("./data/mesh_term_freq_vals.csv", "r") as handle:
-#    for line in handle:
-#        line = line.strip("\n").split(",")
-#        term_freqs[line[0]] = int(line[1])
+term_freqs = {}
+with open("./data/mesh_term_freq_vals.csv", "r") as handle:
+    for line in handle:
+        line = line.strip("\n").split(",")
+        term_freqs[line[0]] = int(line[1])
 #################################################
 
 # Get all root UIDs
@@ -122,32 +126,41 @@ for term in term_trees:
         if len(tree.split(".")) == 1:
             roots[tree] = term
 
+# Computing aggregate information content is done in a step-by-step
+# process here to make it easy to follow along. I used Song, Li, Srimani,
+# Yu, and Wang's paper, "Measure the Semantic Similarity of GO Terms Using
+# Aggregate Information Content" as a guide
+            
 # Get term probs
 term_probs = {uid:-1 for uid in uids}
-for term in term_probs.keys():
+for term in term_probs:
     term_roots = [tree.split(".")[0] for tree in term_trees[term]]
     
     probs = []
     for root in term_roots:
-        if term_freqs[roots[root]] != 0:
-            probs.append(term_freqs[term] / term_freqs[roots[root]])
-        elif term_freqs[term] == 0 and term_freqs[roots[root]] == 0:
-            probs.append(0)
-        elif term_freqs[term] != 0 and term_freqs[roots[root]] == 0:
-            logger.error("Bad div by 0: {}".format(term))
+        try:
+            if term_freqs[roots[root]] != 0:
+                probs.append(term_freqs[term] / term_freqs[roots[root]])
+            elif term_freqs[term] == 0 and term_freqs[roots[root]] == 0:
+                probs.append(0)
+        except ZeroDivisionError:
+            logger.error("Bad div by 0 at term_probs compute step: {}".format(term))
     term_probs[term] = sum(probs) / len(probs)
 
 # Compute IC values
 ics = {uid:np.NaN for uid in uids}
-for term in ics.keys():
+for term in ics:
     if term_probs[term] != 0:
         ics[term] = -1 * math.log(term_probs[term])
 
 # Compute knowledge for each term
 knowledge = {uid:np.NaN for uid in uids}
 for term in knowledge:
-    if ics[term] is not np.NaN and ics[term] != 0:
-        knowledge[term] = 1 / ics[term]
+    try:
+        if ics[term] is not np.NaN:
+            knowledge[term] = 1 / ics[term]
+    except ZeroDivisionError:
+        logger.error("Bad div by 0 at knowledge compute step: {}".format(term))
         
 # Compute semantic weight for each term
 sws = {uid:np.NaN for uid in uids}
@@ -155,3 +168,16 @@ for term in sws:
     if knowledge[term] is not np.NaN:
         sws[term] = 1 / (1 + math.exp(-1 * knowledge[term]))
     
+# Compute semantic value for each term by adding the semantic weights
+# of all its ancestors
+svs = {uid:np.NaN for uid in uids}
+for term in svs:
+    sv = 0
+    ancestors = [".".join(tree.split(".")[:-1]) for tree in term_trees[term]]
+    ancestors = [ancestor for ancestor in ancestors if ancestor]
+    while ancestors:
+        sv += sws[term_trees_rev[ancestors[0]]]
+        ancestors.extend([".".join(tree.split(".")[:-1]) for tree in term_trees[term_trees_rev[ancestors[0]]]])
+        ancestors = [ancestor for ancestor in ancestors if ancestor]
+        ancestors = ancestors[1:]
+    svs[term] = sv
