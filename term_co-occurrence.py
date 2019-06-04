62724069b7ea79e1e0a382e8851d4c40c23713e3
Kirk Gasper <wigasper@gmail.com>
2019-06-04 14:45:18 -0500

    added more logging, debugging memory issues

--- term_co-occurrence.py
+++ term_co-occurrence.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+#import gc
 import json
 import logging
 import traceback
@@ -10,7 +11,7 @@ from bs4 import BeautifulSoup
 # Set up logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-handler = logging.FileHandler("term_co-occurrence.log")
+handler = logging.FileHandler("./logs/term_co-occurrence.log")
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
@@ -26,16 +27,24 @@ docs = os.listdir("./pubmed_bulk")
 
 doc_terms = {}
 
+logger.info("Starting doc/term counting")
 for doc in tqdm(docs):
     try:
-        with open("./pubmed_bulk/{}".format(doc), "r") as handle:
+        with open(f"./pubmed_bulk/{doc}", "r") as handle:
+            logger.info(f"Processing start - {doc}")
             soup = BeautifulSoup(handle.read())
-            for article in soup.find_all("pubmedarticle"):
+            logger.info(f"{doc} loaded - moving to term extract")
+            for article in tqdm(soup.find_all("pubmedarticle")):
                 term_ids = []
                 for mesh_heading in article.find_all("meshheading"):
                     if mesh_heading.descriptorname is not None:
                         term_ids.append(mesh_heading.descriptorname['ui'])
-                doc_terms[article.pubmeddata.articleidlist.articleid.string] = term_ids
+                if article.pubmeddata.articleidlist is not None:
+                    doc_terms[str(article.medlinecitation.pmid.string.extract())] = term_ids
+            logger.info(f"Processing complete - {doc}")
+#        soup.decompose()
+#        gc.collect()
+        
     except Exception as e:
         trace = traceback.format_exc()
         logger.error(repr(e))
