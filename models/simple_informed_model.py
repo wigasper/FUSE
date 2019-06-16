534d135cb10479c13afc334ab8e66d6da494cf6c
Kirk Gasper <wigasper@gmail.com>
2019-06-16 16:49:53 -0500

    added logging/out write for overnight run

--- models/simple_informed_model.py
+++ models/simple_informed_model.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import json
+import logging
+import traceback
 
 from tqdm import tqdm
 import numpy as np 
@@ -16,6 +18,14 @@ def array_builder(filepath, term_idxs):
     return array_out
 
 def main():   
+    # Set up logging
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler("../logs/simple_informed_model.log")
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    
     # Load in term frequencies
     with open("../data/term_freqs.json", "r") as handle:
         temp = json.load(handle)
@@ -43,33 +53,43 @@ def main():
     coocc_log_ratios = array_builder("../data/term_co-occ_log_likelihoods.csv", term_idxs)
     max_ratio = np.max(coocc_log_ratios)
     
+    logger.info("Beginning semantic similarity and co-occurrence incorporation")
     for doc in tqdm(term_freqs.keys()):
-        # add semantically similar terms to each pool and weight by similarity
-        similar_terms = {}
-        coocc_terms = {}
-        for term in term_freqs[doc].keys():
-            if term in term_idxs.keys():
-                row = term_idxs[term]
-                # coocc_log_ratios must have same dims here, may need to do something about this
-                for col in range(sem_sims.shape[0]):
-                    if sem_sims[row, col] > .5 :
-                        similar_terms[term_idxs_reverse[col]] = sem_sims[row,col] * term_freqs[doc][term]
-                    if coocc_log_ratios[row, col] > 5:
-                        coocc_terms[term_idxs_reverse[col]] = (coocc_log_ratios[row, col] / max_ratio) * term_freqs[doc][term]
-        for term in similar_terms.keys():
-            if term in term_freqs[doc].keys():
-                term_freqs[doc][term] += similar_terms[term]
-            else:
-                term_freqs[doc][term] = similar_terms[term]
+        try:
+            # add semantically similar terms to each pool and weight by similarity
+            similar_terms = {}
+            coocc_terms = {}
+            for term in term_freqs[doc].keys():
+                if term in term_idxs.keys():
+                    row = term_idxs[term]
+                    # coocc_log_ratios must have same dims here, may need to do something about this
+                    for col in range(sem_sims.shape[0]):
+                        if sem_sims[row, col] > .5:
+                            similar_terms[term_idxs_reverse[col]] = sem_sims[row,col] * term_freqs[doc][term]
+                        if coocc_log_ratios[row, col] > 5:
+                            coocc_terms[term_idxs_reverse[col]] = (coocc_log_ratios[row, col] / max_ratio) * term_freqs[doc][term]
+            for term in similar_terms.keys():
+                if term in term_freqs[doc].keys():
+                    term_freqs[doc][term] += similar_terms[term]
+                else:
+                    term_freqs[doc][term] = similar_terms[term]
+    
+            for term in coocc_terms.keys():
+                if term in term_freqs[doc].keys():
+                    term_freqs[doc][term] += coocc_terms[term]
+                else:
+                    term_freqs[doc][term] = coocc_terms[term]
+        except Exception as e:
+            trace = traceback.format_exc()
+            logger.error(repr(e))
+            logger.critical(trace)
+    logger.info("Semantic similarity and co-occurrence incorporation complete")
+    
+    logger.info("Writing output")
+    with open("../data/term_freqs_w_semsim_termcoocc.json", "w") as out:
+        json.dump(term_freqs, out)
 
-        for term in coocc_terms.keys():
-            if term in term_freqs[doc].keys():
-                term_freqs[doc][term] += coocc_terms[term]
-            else:
-                term_freqs[doc][term] = coocc_terms[term]
-
-
-
+"""
 
 ############################################
                 ############################
@@ -89,110 +109,4 @@ def main():
             else:
                 term_freqs[doc][term] = similar_terms[term]
     
-    # before:
-    term_freqs["21502780"]
-    {'D000063': 0.05405405405405406,
-     'D000328': 0.08108108108108109,
-     'D001334': 0.08108108108108109,
-     'D040421': 0.08108108108108109,
-     'D005260': 0.08108108108108109,
-     'D006801': 0.08108108108108109,
-     'D008297': 0.08108108108108109,
-     'D008875': 0.08108108108108109,
-     'D012307': 0.02702702702702703,
-     'D014739': 0.05405405405405406,
-     'D000293': 0.02702702702702703,
-     'D000368': 0.02702702702702703,
-     'D018592': 0.02702702702702703,
-     'D004636': 0.02702702702702703,
-     'D016016': 0.02702702702702703,
-     'D018570': 0.02702702702702703,
-     'D013997': 0.02702702702702703,
-     'D014904': 0.02702702702702703,
-     'D017677': 0.02702702702702703,
-     'D012309': 0.02702702702702703,
-     'D017678': 0.02702702702702703}
-    
-    
-    
-    
-    
-    
-    
-    
-    # The baseline model. This model will predict a term if its frequency
-    # is greater than the threshold
-    thresholds = [x * .005 for x in range(0,200)]
-    
-    predictions = {}
-    precisions = []
-    recalls = []
-    f1s = []
-    
-    # Run the model for all thresholds
-    for thresh in thresholds:
-        # Predict
-        for doc in tqdm(term_freqs):
-            term_pool = [t for term in doc[1].keys() if term in sem_sims.keys() for t in sem_sims[term]]
-            term_pool.extend(doc[1].keys())
-            term_pool = list(dict.fromkeys(term_pool))
-            pred = []
-            for term in term_pool:
-                term_val = []
-                if term in doc[1].keys():
-                    term_val.append(doc[1][term])
-                if term in sem_sims.keys():
-                    term_val.extend([doc[1][t] for t in doc[1].keys() if t in sem_sims[term]])
-                term_val = sum(term_val)
-                if term_val > thresh:
-                    pred.append(term)
-            predictions[doc[0]] = pred
-            
-        # Get evaluation metrics
-        true_pos = 0
-        false_pos = 0
-        false_neg = 0
-        
-        for pmid in predictions.keys():
-            true_pos += len([pred for pred in predictions[pmid] if pred in solution[pmid]])
-            false_pos += len([pred for pred in predictions[pmid] if pred not in solution[pmid]])
-            false_neg += len([sol for sol in solution[pmid] if sol not in predictions[pmid]])
-    
-        if true_pos == 0:
-            precision = 0
-            recall = 0
-            f1 = 0
-        else:
-            precision = true_pos / (true_pos + false_pos)
-            recall = true_pos / (true_pos + false_neg)
-            f1 = (2 * precision * recall) / (precision + recall)
-    
-        print("thresh: " + str(thresh))
-        print("precision: " + str(precision))
-        print("recall: " + str(recall))
-        print("f1: " + str(f1))
-    
-        precisions.append(precision)
-        recalls.append(recall)
-        f1s.append(f1)
-            
-    from sklearn.metrics import auc
-    from matplotlib import pyplot
-    with open("../data/baseline_sem_sim_corr_results_2", "w") as out:
-        out.write("".join(["auc: ", str(auc(recalls, precisions))]))
-        out.write("\n")
-        out.write("f1s: ")
-        out.write(",".join([str(f1) for f1 in f1s]))
-        out.write("\n")
-        out.write("precisions: ")
-        out.write(",".join([str(prec) for prec in precisions]))
-        out.write("\n")
-        out.write("recalls: ")
-        out.write(",".join([str(rec) for rec in recalls]))
-    
-    # AUC
-    #print("AUC: ", auc(recalls, precisions))
-    pyplot.plot([0, 1], [0.5, 0.5], linestyle="--")
-    pyplot.plot(recalls, precisions, marker=".")
-    pyplot.savefig("../pr_curve.png")
-    pyplot.show()
+"""
