47833ce69d76767ba3f419c6d8f1f5b6f79bf2d3
Kirk Gasper <wigasper@gmail.com>
2019-06-27 11:09:55 -0500

    abstracted work to functions, added argparse, removed max, added default learner

--- models/threshold_learner.py
+++ models/threshold_learner.py
@@ -2,12 +2,14 @@
 import json
 import time
 import logging
+import argparse
 from multiprocessing import Process, Queue
 from copy import deepcopy
 
 # MP worker that calculates the optimately discrimination threshold
 # for a single UID
 def uid_worker(work_queue, write_queue, term_freqs, solution):
+    # optimize this
     thresholds = [x * .005 for x in range(0,200)]
 
     while True:
@@ -62,112 +64,66 @@ def dict_writer(write_queue, completed_queue, uids):
             break
         thresholds_dict[result[0]] = result[1]
 
-def main():
-    # Set up logging
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-    handler = logging.FileHandler("../logs/learn_individ_thresholds_mp.log")
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-
-    # Load in term frequencies and partition
-    with open("../data/term_freqs.json", "r") as handle:
-        temp = json.load(handle)
-
-    train = temp[0:10000]
-    test = temp[10000:]
-
-    term_freqs = {}
-    for doc in train:
-        term_freqs[doc[0]] = doc[1]
+def learn_default_threshold(term_freqs, solution):
+    thresholds = [x * .005 for x in range(0,200)]
     
-    test_freqs = {}
-    for doc in test:
-        test_freqs[doc[0]] = doc[1]
-
-    # Load in solution values
-    with open("../data/baseline_solution.json", "r") as handle:
-        solution = json.load(handle)
-
-    # Load in UIDs
-    uids = []
-    with open("../data/mesh_data.tab", "r") as handle:
-        for line in handle:
-            line = line.strip("\n").split("\t")
-            uids.append(line[0])
-
-    # Get max freq for docs with more than 10 terms applied to their citations
-    max_freq = 0
-    for doc in term_freqs:
-        for term in term_freqs[doc].keys():
-            if len(term_freqs[doc]) > 10 and term_freqs[doc][term] > max_freq:
-                max_freq = term_freqs[doc][term]
-
-    # Divide values by max
-    for doc in term_freqs:
-        for term in term_freqs[doc].keys():
-            term_freqs[doc][term] = term_freqs[doc][term] / max_freq
+    predictions = {}
+    precisions = []
+    recalls = []
+    f1s = []
+    
+    # Run the model for all thresholds
+    for thresh in thresholds:
+        # Predict
+        for doc in term_freqs.keys():
+            predictions[doc] = [key for key, val in term_freqs[doc].items() if val > thresh]
             
-    # Dict to store the optimal thresholds for each UID
-    #uid_thresholds = {uid: 0 for uid in uids}
-
-    # MP architecture
-    num_workers = 6
-    # this is sort of ready to increase num writers but currently 
-    # hacked together and num_writers should not be more than 1
-    # unless dict (from completed queue) joining logic is added at the end
-    # and deep copies are passed to writers
-    num_writers = 1
-    work_queue = Queue(maxsize=100)
-    write_queue = Queue(maxsize=100)
-    completed_queue = Queue(maxsize=10)
-
-    writers = [Process(target=dict_writer, args=(write_queue, completed_queue, 
-                uids)) for _ in range(num_writers)]
-
-    for writer in writers:
-        writer.daemon = True
-        writer.start()
-
-    workers = [Process(target=uid_worker, args=(work_queue, write_queue, 
-                deepcopy(term_freqs), deepcopy(solution))) for _ in range(num_workers)]
-
-    for worker in workers:
-        worker.start()
-
-    counter = 0
-    logging_interval = 100
-    for uid in uids:
-        if counter % logging_interval == 0:
-            logger.info(f"{counter} UIDs added to queue")
-        work_queue.put(uid)
-        counter += 1
-
-    while True:
-        if work_queue.empty():
-            for _ in range(num_workers):
-                work_queue.put(None)
-            break
+        # Get evaluation metrics
+        true_pos = 0
+        false_pos = 0
+        false_neg = 0
+        
+        for pmid in predictions:
+            true_pos += len([pred for pred in predictions[pmid] if pred in solution[pmid]])
+            false_pos += len([pred for pred in predictions[pmid] if pred not in solution[pmid]])
+            false_neg += len([sol for sol in solution[pmid] if sol not in predictions[pmid]])
     
-    for worker in workers:
-        worker.join()
-
-    for writer in writers:
-        write_queue.put(None)
-
-    if write_queue.empty():
-        uid_thresholds = completed_queue.get()
-        for writer in writers:
-            writer.join()
-
-    # Save training results
-    with open ("../data/individual_term_thresholds.json", "w") as out:
-        json.dump(uid_thresholds, out)
+        if true_pos == 0:
+            precision = 0
+            recall = 0
+            f1 = 0
+        else:
+            precision = true_pos / (true_pos + false_pos)
+            recall = true_pos / (true_pos + false_neg)
+            f1 = (2 * precision * recall) / (precision + recall)
+        
+        precisions.append(precision)
+        recalls.append(recall)
+        f1s.append(f1)
     
+    return thresholds[f1s.index(max(f1s))]
+
+def predict(test_freqs, solution):
+    # Get default threshold value, in case training data is missing 
+    # a class entirely or threshold value is too high (learn this - what is too high???)
+    default_thresh = learn_default_threshold(test_freqs, solution)
+
+    uid_thresholds = {}
+    # Load training results
+    with open("../data/individual_term_thresholds.csv", "r") as handle:
+        for line in handle:
+            line = line.strip("\n").split(",")
+            if line[1] == 0: # or is greater than max?
+                line[1] == default_thresh
+            uid_thresholds[line[0]] = float(line[1])
+    
+    for term in uid_thresholds:
+        if uid_thresholds[term] ==  0:
+            uid_thresholds[term] = .09
+
     # Test it out
     predictions = {}
-
+            
     # Predict
     for doc in test_freqs.keys():
         predictions[doc] = [key for key, val in test_freqs[doc].items() if val > uid_thresholds[key]]
@@ -192,7 +148,122 @@ def main():
         f1 = (2 * precision * recall) / (precision + recall)
 
     from notify import notify     
-    notify(f"all done, precision: {precision}, recall: {recall}, f1: {f1}")  
+    notify(f"all done, precision: {precision}, recall: {recall}, f1: {f1}")
+
+    return predictions
+
+def train(train_freqs, solution, logger):
+    # Load in UIDs
+    uids = []
+    with open("../data/mesh_data.tab", "r") as handle:
+        for line in handle:
+            line = line.strip("\n").split("\t")
+            uids.append(line[0])
+
+    # MP architecture
+    num_workers = 6
+    # this is sort of ready to increase num writers but currently 
+    # hacked together and num_writers should not be more than 1
+    # unless dict (from completed queue) joining logic is added at the end
+    num_writers = 1
+    work_queue = Queue(maxsize=100)
+    write_queue = Queue(maxsize=100)
+    completed_queue = Queue(maxsize=10)
+
+    writers = [Process(target=dict_writer, args=(write_queue, completed_queue, 
+                uids)) for _ in range(num_writers)]
+
+    for writer in writers:
+        writer.daemon = True
+        writer.start()
+
+    workers = [Process(target=uid_worker, args=(work_queue, write_queue, 
+                deepcopy(train_freqs), deepcopy(solution))) for _ in range(num_workers)]
+
+    for worker in workers:
+        worker.start()
+
+    counter = 0
+    logging_interval = 100
+    for uid in uids[0:100]:
+        if counter % logging_interval == 0:
+            logger.info(f"{counter} UIDs added to queue")
+        work_queue.put(uid)
+        counter += 1
+
+    while True:
+        if work_queue.empty():
+            for _ in range(num_workers):
+                work_queue.put(None)
+            break
+    
+    for worker in workers:
+        worker.join()
+
+    for writer in writers:
+        write_queue.put(None)
+
+    if write_queue.empty():
+        uid_thresholds = completed_queue.get()
+        for writer in writers:
+            writer.join()
+
+    default_thresh = learn_default_threshold(train_freqs, solution)
+
+    for uid in uid_thresholds:
+        if uid_thresholds[uid] == 0: # need to learn ceiling here too!!!
+            uid_thresholds[uid] = default_thresh
+
+    # Save training results
+    with open("../data/individual_term_thresholds.csv", "w") as out:
+        for uid in uid_thresholds:
+            out.write("".join([uid, ",", str(uid_thresholds[uid]), "\n"]))
+
+def main():
+    # Get command line args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--train", help="train model", action="store_true")
+    parser.add_argument("-p", "--predict", help="predict", action="store_true")
+    parser.add_argument("-i", "--input", help="input file path with term freqs for each doc", type=str)
+    args = parser.parse_args()
+
+    # Set up logging
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler("../logs/learn_individ_thresholds_mp.log")
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    # Load in term frequencies and partition
+    if args.input:
+        with open(args.input, "r") as handle:
+            temp = json.load(handle)
+    else:
+        with open("../data/term_freqs.json", "r") as handle:
+            temp = json.load(handle)
+
+    partition = int(len(temp) * .8)
+    train_temp = temp[0:partition]
+    test_temp = temp[partition:]
+
+    train_freqs = {}
+    for doc in train_temp:
+        train_freqs[doc[0]] = doc[1]
+    
+    test_freqs = {}
+    for doc in test_temp:
+        test_freqs[doc[0]] = doc[1]
+
+    # Load in solution values
+    with open("../data/baseline_solution.json", "r") as handle:
+        solution = json.load(handle)
+
+    if args.train:
+        train(train_freqs, solution, logger)
+    
+    if args.predict:
+        preds = predict(test_freqs, solution)
 
 if __name__ == "__main__":
     main()
