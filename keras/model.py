d6cd345107affc5374cc5ab69857fb0e276c9e26
William Gasper <wkg@williamgasper.com>
2020-05-11 16:00:55 -0500

    added decision threshold learning on validation

--- keras/model.py
+++ keras/model.py
@@ -34,7 +34,7 @@ def get_model(dim):
     return mod
 
 @jit
-def count_metrics(y, y_hat, threshold):
+def count_metrics(y, y_hat, thresholds):
     true_pos = 0
     false_pos = 0
     false_neg = 0
@@ -46,7 +46,8 @@ def count_metrics(y, y_hat, threshold):
         for col in range(n_cols):
             y_hat_val = y_hat[row][col]
             y_val = y[row][col]
-            
+            threshold = thresholds[col]
+
             if y_hat_val > threshold and y_val == 1.0:
                 true_pos += 1
             elif y_hat_val <= threshold and y_val == 1.0:
@@ -56,8 +57,11 @@ def count_metrics(y, y_hat, threshold):
             
     return true_pos, false_pos, false_neg
 
-def test(weights_fp, logger, threshold, mod=get_model(2048)):
+def test(weights_fp, logger, threshold, mod=get_model(2048), decision_thresholds):
     mod.load_weights(weights_fp)
+
+    # this is a dict, need to make a list for better numba usage
+    thresholds = [decision_thresholds[resp] for resp in range(len(decision_thresholds))]
 
     test_ids = []
     with open("test_ids", "r") as handle:
@@ -77,7 +81,7 @@ def test(weights_fp, logger, threshold, mod=get_model(2048)):
 
         y_hat = mod.predict_on_batch(x)
         
-        tp_temp, fp_temp, fn_temp = count_metrics(y, y_hat, threshold)
+        tp_temp, fp_temp, fn_temp = count_metrics(y, y_hat, thresholds)
         true_pos += tp_temp
         false_pos += fp_temp
         false_neg += fn_temp
@@ -91,6 +95,88 @@ def test(weights_fp, logger, threshold, mod=get_model(2048)):
 
     logger.info(f"F1: {f1}, precision: {precision}, recall: {recall}, threshold: {threshold}")
 
+@jit
+def get_f1(threshold, y, yhat):
+    true_pos = 0
+    false_pos = 0
+    false_neg = 0
+    
+    y_len = len(y)
+
+    for idx in range(y_len):
+        y_hat_val = y_hat[idx]
+        y_val = y[idx]
+            
+        if y_hat_val > threshold and y_val == 1.0:
+            true_pos += 1
+        elif y_hat_val <= threshold and y_val == 1.0:
+            false_neg += 1
+        elif y_hat_val > threshold and y_val == 0.0:
+            false_pos += 1
+
+    if true_pos == 0:
+        precision = 0
+        recall = 0
+        f1 = 0
+    else:
+        precision = true_pos / (true_pos + false_pos)
+        recall = true_pos / (true_pos + false_neg)
+        f1 = (2 * precision * recall) / (precision + recall)
+
+    return f1
+
+def find_decision_thresholds(validation_ids, weights_fp, logger, mod):
+    # first need to save arrays with y and yhat
+    mod.load_weights(weights_fp)
+    batch_size = 16
+    test_gen = DataGen(validation_ids, batch_size)
+    default_thresh = 0.33
+    
+    batch_num = 0
+    batch_ids = []
+
+    logger.info("Generating predictions and saving")
+    for batch in tqdm(test_gen):
+        x = batch[0]
+        y = batch[1]
+
+        y_hat = mod.predict_on_batch(x)
+        
+        np.save(f"y/{batch_num}_y.npy", y)
+        np.save(f"y/{batch_num}_y_hat.npy", y_hat)
+        batch_ids.append(batch_num)
+        batch_num += 1
+    
+    responses = {num: default_thresh for num in range(29351)}
+
+    thresholds = [x * .01 for x in range(100)]
+
+    logger.info("Determining optimal thresholds for each response")
+    for resp in tqdm(responses):
+        ys = []
+        y_hats = []
+
+        for batch in batch_ids:
+            y = np.load(f"y/{batch_num}_y.npy")
+            y_hat = np.load(f"y/{batch_num}_y_hat.npy") 
+            
+            ys.extend([val for val in y[:,resp]])
+            y_hats.extend([val for val in y_hat[:,resp]])
+        
+        f1s = []
+        for idx, threshold in enumerate(thresholds):
+            f1s.append(get_f1(threshold, y, y_hat))
+            # early stopping
+            if f1s[-10] > f1s[-1]:
+                break
+
+        responses[resp] = thresholds[f1s.index(max(f1s))]
+
+    with open("decision_thresholds.json", "w") as out:
+        json.dump(responses, out)
+
+    return responses
+
 if __name__ == "__main__":
     logger = get_logger()
     
@@ -99,6 +185,9 @@ if __name__ == "__main__":
         for line in handle:
             ids_list.append(line.strip("\n"))
     
+    validation_ids = ids_list[400000:]
+    ids_list = ids_list[:400000]
+
     epochs = 30
     batch_size = 16
     training_generator = DataGen(ids_list, batch_size) 
@@ -107,16 +196,16 @@ if __name__ == "__main__":
     logger.info(f"training on all ids, expanded training set. {epochs} epochs")
 
     #dims = [100, 200, 400, 800, 1600, 2000]
-    # 5000 current best, excluding now for testing
-    dims = [1600]
-    for dim in dims:
-        logger.info(f"training model with {dim}, {dim} dimension dense")
-        mod = get_model(dim)
-        mod.fit(training_generator, use_multiprocessing=True, workers=4,
-                epochs=epochs)
-        mod.save_weights(fp)
-        
-        thresholds = [0.20, 0.21, 0.22, 0.23, 0.24, 0.24, 0.26, 0.27, 0.28]
-        for thresh in thresholds:
-            test(fp, logger, thresh, mod)    
-            tf.keras.backend.clear_session()
+    dim = 1600
+    #for dim in dims:
+    logger.info(f"training model with {dim}, {dim} dimension dense")
+    mod = get_model(dim)
+    mod.fit(training_generator, use_multiprocessing=True, workers=4,
+            epochs=epochs)
+    mod.save_weights(fp)
+    
+    # good default decision threshold is 0.33 
+    # move to validation, need to learn an optimal decision threshold for each class
+    find_decision_thresholds(validation_ids, weights_fp, logger, mod)
+
+    test(weights_fp, logger, threshold, mod, decision_thresholds)
